8f78163803a9897f154faf08325e7627848be636
Olivia Cubela <oliviacubela@gmail.com>
2020-09-09 00:22:56 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,7 +28,7 @@ def can_be_created_in_a_block(title="Home Alone", release_date=1990)
   # release_date == 1990
 
   Movie.create do |m|
-    m.title = "Home Alone"
+    m.title = "#{title}"
     m.release_date = 1990
   end
 end
