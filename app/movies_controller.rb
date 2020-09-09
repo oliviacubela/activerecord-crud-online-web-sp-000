8bf1bf2309a148c3bb0f7c02994d9568d06e6d69
Olivia Cubela <oliviacubela@gmail.com>
2020-09-09 00:21:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -83,7 +83,7 @@ end
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
-  __
+  movie.destroy(title: "That One Where the Guy Kicks Another Guy Once")
 end
 
 def can_destroy_all_items_at_once
